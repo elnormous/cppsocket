4420d58c6719a6f54bff8b503201996c74b38ea9
Elviss Strazdins <elviss@elviss.lv>
2016-10-13 03:43:31 +0300

    Move Log destructor to Log.cpp

--- Log.cpp
+++ Log.cpp
@@ -2,6 +2,17 @@
 //  cppsocket
 //
 
+#include <iostream>
+#include <string>
+#ifdef _MSC_VER
+#include <windows.h>
+#include <strsafe.h>
+#else
+    #if defined(LOG_SYSLOG)
+    #include <sys/syslog.h>
+    #endif
+#endif
+
 #include "Log.h"
 
 namespace cppsocket
@@ -11,4 +22,40 @@ namespace cppsocket
 #else
     Log::Level Log::threshold = Log::Level::ALL;
 #endif
+
+    Log::~Log()
+    {
+        if (level <= threshold)
+        {
+            if (level == Level::ERR ||
+                level == Level::WARN)
+            {
+                std::cerr << s.str() << std::endl;
+            }
+            else
+            {
+                std::cout << s.str() << std::endl;
+            }
+        }
+
+#ifdef _MSC_VER
+        wchar_t szBuffer[MAX_PATH];
+        MultiByteToWideChar(CP_UTF8, 0, s.str().c_str(), -1, szBuffer, MAX_PATH);
+        StringCchCatW(szBuffer, sizeof(szBuffer), L"\n");
+        OutputDebugStringW(szBuffer);
+#else
+#if defined(LOG_SYSLOG)
+        int priority = 0;
+        switch (level)
+        {
+            case ERR: priority = LOG_ERR; break;
+            case WARN: priority = LOG_WARNING; break;
+            case INFO: priority = LOG_INFO; break;
+            case DEBUG: priority = LOG_DEBUG; break;
+            default: break;
+        }
+        syslog(prio, "%s", s.str().c_str());
+#endif
+#endif
+    }
 }
--- Log.h
+++ Log.h
@@ -4,17 +4,7 @@
 
 #pragma once
 
-#include <iostream>
 #include <sstream>
-#include <string>
-#ifdef _MSC_VER
-#include <windows.h>
-#include <strsafe.h>
-#else
-    #if defined(LOG_SYSLOG)
-    #include <sys/syslog.h>
-    #endif
-#endif
 
 namespace cppsocket
 {
@@ -40,41 +30,7 @@ namespace cppsocket
         {
         }
 
-        ~Log()
-        {
-            if (level <= threshold)
-            {
-                if (level == Level::ERR ||
-                    level == Level::WARN)
-                {
-                    std::cerr << s.str() << std::endl;
-                }
-                else
-                {
-                    std::cout << s.str() << std::endl;
-                }
-            }
-
-#ifdef _MSC_VER
-            wchar_t szBuffer[MAX_PATH];
-            MultiByteToWideChar(CP_UTF8, 0, s.str().c_str(), -1, szBuffer, MAX_PATH);
-            StringCchCatW(szBuffer, sizeof(szBuffer), L"\n");
-            OutputDebugStringW(szBuffer);
-#else
-    #if defined(LOG_SYSLOG)
-            int priority = 0;
-            switch (level)
-            {
-                case ERR: priority = LOG_ERR; break;
-                case WARN: priority = LOG_WARNING; break;
-                case INFO: priority = LOG_INFO; break;
-                case DEBUG: priority = LOG_DEBUG; break;
-                default: break;
-            }
-            syslog(prio, "%s", s.str().c_str());
-    #endif
-#endif
-        }
+        ~Log();
 
         template <typename T> Log& operator << (T val)
         {
