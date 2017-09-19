f8c51cef57ab02448adb16758cf4cfd4b47e9f36
Elviss Strazdins <elviss@elviss.lv>
2017-09-20 01:37:35 +0300

    Convert the IP address to host endianness

--- Socket.hpp
+++ Socket.hpp
@@ -28,10 +28,10 @@ namespace cppsocket
     {
         uint8_t* ptr = reinterpret_cast<uint8_t*>(&ip);
 
-        return std::to_string(static_cast<uint32_t>(ptr[0])) + "." +
-        std::to_string(static_cast<uint32_t>(ptr[1])) + "." +
+        return std::to_string(static_cast<uint32_t>(ptr[3])) + "." +
         std::to_string(static_cast<uint32_t>(ptr[2])) + "." +
-        std::to_string(static_cast<uint32_t>(ptr[3]));
+        std::to_string(static_cast<uint32_t>(ptr[1])) + "." +
+        std::to_string(static_cast<uint32_t>(ptr[0]));
     }
 
     inline int getLastError()
